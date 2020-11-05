5264f77f22634c5986288fceb3ae7491010f607a
Alane-Tc <alantamez28@hotmail.com>
2020-11-04 22:39:42 -0600

    Update app

--- app/src/main/java/com/tecmilenio/contactos/MainActivity.kt
+++ app/src/main/java/com/tecmilenio/contactos/MainActivity.kt
@@ -7,12 +7,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Bnt_Confirmacion.setOnClickListener(){
-            val intent: Intent = Intent(this, Menu::class.java);
+        Add_contact.setOnClickListener(){
+            val intent: Intent = Intent(this, Add::class.java);
             startActivity(intent);
         }
+
+        Show_contact.setOnClickListener(){
+            val intent: Intent = Intent(this, EditContact::class.java);
+            startActivity(intent);
+        }
+
+
     }
-}
+    }
